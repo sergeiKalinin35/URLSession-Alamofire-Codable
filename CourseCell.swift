046868b7ab594e847ffaf3845fe4155c3d55576a
Sergey <piolersa85@mail.ru>
2021-08-17 03:14:13 +0200

    sun

--- CourseCell.swift
+++ CourseCell.swift
@@ -16,23 +16,24 @@ class CourseCell: UITableViewCell {
     @IBOutlet var numberOfTest: UILabel!
     
     // создаем публичный метод для передачи оутлетов
-    public func configure(with course: Course) {
+     func configure(with course: Course) {
         courseNameLabel.text = course.name
         numberOfLesson.text = "Number of lessons: \(course.number_of_lessons ?? 0)"
         numberOfTest.text = "Number of lessons: \(course.number_of_tests ?? 0)"
         
         
         // загрузка картинки одной строкой
-        guard let stringURL = course.imageUrl else { return }// извлекаем строку URL адрес
-        guard let imageURL = URL(string: stringURL) else { return }
-        guard let imageData = try? Data(contentsOf: imageURL) else { return }// сетевой запрос уходим с этой строки в фоновый режим
-        
-        DispatchQueue.main.async {
-            self.courseImage.image = UIImage(data: imageData)
+        DispatchQueue.global().async {
+            guard let stringURL = course.imageUrl else { return }// извлекаем строку URL адрес
+            guard let imageURL = URL(string: stringURL) else { return }
+            guard let imageData = try? Data(contentsOf: imageURL) else { return }// сетевой запрос уходим с этой строки в фоновый режим
+            
+            DispatchQueue.main.async {
+                self.courseImage.image = UIImage(data: imageData)
+            }
+            
         }
-        
-        
-        
+       
     }
     
 }
